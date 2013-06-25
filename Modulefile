533abc04fec336bc4ebc2592cdb778d1bea97478
Jarl Stefansson <jarl.stefansson@gmail.com>
2013-06-25 15:24:38 -0400

    Inital config

---
+++ Modulefile
@@ -0,0 +1,8 @@
+name    'jalli-bind-utils'
+version '0.0.1'
+source 'http://github.com/jalli/puppet-module-bind-utils'
+author 'jalli'
+license 'GNU General Public License, Version 3.0'
+summary 'Setup bind-utils/dnsutils'
+description 'Sets up bind-utils on redhat based systems (Fedora, Centos, RHEL, etc) or dnsutils on debian based systems (Ubuntu, Mint, Debian)
+project_page 'http://github.com/jalli/puppet-module-bind-utils'
---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+# == Class: bind-utils
+#
+# Setup bind-utils on redhat based systems (Centos, Fedora, RHEL, etc) or dnsutils on debian based systems (Ubuntu, Mint, Debian, etc)
+#
+# === Authors
+#
+# Author Name <jarl.stefansson@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2013 Jarl Stefansson, unless otherwise noted.
+#
+class jalli-bind-utils {
+  case $osfamily {
+    debian: {
+      $package_name = 'dnsutils'
+    }
+    redhat: {
+      $package_name = 'bind-utils'
+    }
+    default: {
+      case $operatingsystem {
+        default: {
+          fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
+        }
+      }
+    }
+  }
+
+  package { $package_name:
+    ensure => installed,
+  }
+}
